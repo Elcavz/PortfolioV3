720a0abaed59f70c024588b5a142d4469aac1dc9
Elcavz <89607443+Elcavz@users.noreply.github.com>
2023-09-23 01:01:08 +0800

    Portfolio V3 | Responsive w/ Bootstrap 5

--- try.js
+++ try.js
@@ -16,3 +16,121 @@ $(document).ready(function(){
         clickEvent = false;
     });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const menuIcon = document.querySelector(".menu-icon");
+    const sidebar = document.querySelector(".sidebar");
+    const sidebarOverlay = document.querySelector(".sidebar-overlay");
+    const closeSidebar = document.querySelector("#close-sidebar");
+  
+    menuIcon.addEventListener("click", function () {
+      sidebar.classList.add("open");
+      sidebarOverlay.classList.add("show");
+  
+      // Add a delay before applying the rotation class
+      setTimeout(function () {
+        closeSidebar.classList.add("rotate-icon"); // Apply rotation class
+      }, 500); // Adjust the delay time (in milliseconds) as needed
+    });
+  
+    closeSidebar.addEventListener("click", function () {
+      sidebar.classList.remove("open");
+      sidebarOverlay.classList.remove("show");
+  
+      closeSidebar.classList.remove("rotate-icon"); // Remove rotation class
+    });
+  
+    sidebarOverlay.addEventListener("click", function () {
+      sidebar.classList.remove("open");
+      sidebarOverlay.classList.remove("show");
+  
+      closeSidebar.classList.remove("rotate-icon"); // Remove rotation class
+    });
+  });
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const menuIcon = document.querySelector(".menu-icon");
+    const sidebar = document.querySelector(".sidebar");
+    const sidebarOverlay = document.querySelector(".sidebar-overlay");
+    const closeSidebar = document.querySelector("#close-sidebar");
+    const menuContainer = document.querySelector(".menu-container");
+  
+    menuIcon.addEventListener("click", function () {
+      sidebar.classList.add("open");
+      sidebarOverlay.classList.add("show");
+  
+      // Add a delay before applying the rotation class
+      setTimeout(function () {
+        closeSidebar.classList.add("rotate-icon"); // Apply rotation class
+      }, 100); // Adjust the delay time (in milliseconds) as needed
+    });
+  
+    closeSidebar.addEventListener("click", function () {
+      sidebar.classList.remove("open");
+      sidebarOverlay.classList.remove("show");
+  
+      closeSidebar.classList.remove("rotate-icon"); // Remove rotation class
+    });
+  
+    sidebarOverlay.addEventListener("click", function () {
+      sidebar.classList.remove("open");
+      sidebarOverlay.classList.remove("show");
+  
+      closeSidebar.classList.remove("rotate-icon"); // Remove rotation class
+    });
+  
+    // Add a scroll event listener to toggle the background color
+    window.addEventListener("scroll", function () {
+      const scrollPosition = window.scrollY;
+      const viewportHeight = window.innerHeight;
+      const scrollThreshold = viewportHeight * 0.3; // 50% of viewport height
+  
+      if (scrollPosition > scrollThreshold) {
+        menuContainer.classList.add("scrolled");
+      } else {
+        menuContainer.classList.remove("scrolled");
+      }
+    });
+  });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const menuIcon = document.querySelector(".menu-icon");
+  const sidebar = document.querySelector(".sidebar");
+  const sidebarOverlay = document.querySelector(".sidebar-overlay");
+  const closeSidebar = document.querySelector("#close-sidebar");
+  const menuContainer = document.querySelector(".menu-container");
+  const spanElement = document.querySelector(".text-warning");
+
+  menuIcon.addEventListener("click", function () {
+    sidebar.classList.add("open");
+    sidebarOverlay.classList.add("show");
+
+    // Add a delay before applying the rotation class
+    setTimeout(function () {
+      closeSidebar.classList.add("rotate-icon"); // Apply rotation class
+    }, 100); // Adjust the delay time (in milliseconds) as needed
+  });
+
+  closeSidebar.addEventListener("click", function () {
+    sidebar.classList.remove("open");
+    sidebarOverlay.classList.remove("show");
+
+    closeSidebar.classList.remove("rotate-icon"); // Remove rotation class
+  });
+
+  sidebarOverlay.addEventListener("click", function () {
+    sidebar.classList.remove("open");
+    sidebarOverlay.classList.remove("show");
+
+    closeSidebar.classList.remove("rotate-icon"); // Remove rotation class
+  });
+
+  // Add a scroll event listener to change the span color
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 0) {
+      spanElement.classList.add("text-warning-scroll"); // Apply the color change class
+    } else {
+      spanElement.classList.remove("text-warning-scroll"); // Remove the color change class
+    }
+  });
+});
